10401fca217cb6d8d59efdc2baf3ee79a6361015
Nishanth Menon <nm@ti.com>
2023-10-26 19:12:04 -0500

    [NOT FOR UPSTREAM] Add a couple of test scripts
    Lets add a few test scripts for us to easily program the device.
    
    Signed-off-by: Nishanth Menon <nm@ti.com>

---
+++ tcl/test-l.tcl
@@ -0,0 +1,18 @@
+## Using this test script for quick CI:
+#../src/openocd -f board/ti_mspm0_launchpad.cfg -f ./test-l.tcl
+
+# init before running this script - this allows us to provide this as -f option at command line.
+init
+mspm0_board_reset
+sleep 1000
+mspm0_factory_reset
+sleep 1000
+flash info 1
+mspm0_mass_erase
+flash protect 1 0 last off
+flash erase_sector 1 0 last
+program /home/nmenon/tmp/mspm0_images/out_of_box_LP_MSPM0L1306_nortos_ticlang.out verify
+mspm0_start_bootloader
+sleep 1000
+mspm0_board_reset
+shutdown
