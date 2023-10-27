8f0bd59655065bd6822773c091c1952529a7219d
Nishanth Menon <nm@ti.com>
2023-10-26 19:12:04 -0500

    [NOT FOR UPSTREAM] Add a couple of test scripts
    Lets add a few test scripts for us to easily program the device.
    
    Change-Id: Id3ae7e362433e00ff8b0239698d46a0f1463be21
    Signed-off-by: Nishanth Menon <nm@ti.com>

---
+++ tcl/test-g.tcl
@@ -0,0 +1,21 @@
+## Using this test script for quick CI:
+#../src/openocd -f ./test-g.tcl
+#
+adapter serial MG350001
+gdb port 3334
+source [find board/ti_mspm0_launchpad.cfg]
+
+# init before running this script - this allows us to provide this as -f option at command line.
+init
+mspm0_board_reset
+sleep 1000
+mspm0_factory_reset
+sleep 1000
+flash info 0
+mspm0_mass_erase
+flash erase_sector 0 0 last
+program /home/nmenon/tmp/mspm0_images/out_of_box_LP_MSPM0G3507_nortos_ticlang.out verify
+mspm0_start_bootloader
+sleep 1000
+mspm0_board_reset
+shutdown
